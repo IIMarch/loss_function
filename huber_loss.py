fce79694882b8ea2abe7cda6e82d1cf27091b8d1
ScutterKey <370846270@qq.com>
2017-11-22 22:44:32 +0800

    Update huber_loss.py

--- huber_loss.py
+++ huber_loss.py
@@ -6,4 +6,4 @@ def huber_loss(predictions, labels, delta):
     quadratic = mx.sym.minimum(abs_error, delta)
     linear = (abs_error - quadratic)
     losses = 0.5 * quadratic * quadratic + delta * linear
-    return losses
+    return mx.sym.MakeLoss(losses)
